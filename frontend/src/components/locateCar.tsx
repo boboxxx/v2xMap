3dfefebb9be27f20d7353f7fb7b887fc0c7f101f
bobo <1320124900@qq.com>
2024-03-18 10:41:21 +0000

    load maarker

--- frontend/src/components/locateCar.tsx
+++ frontend/src/components/locateCar.tsx
@@ -1,52 +1,45 @@
-//从后端flask获取车辆位置信息后，将车辆位置信息显示在地图上，建立通信，实现车辆位置的实时更新
-import  { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import L, { LatLngTuple } from 'leaflet';
-import { LatLng } from 'leaflet';
+import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
-import { Car } from '../types/Car';
-import { CarIcon } from  'public\car.png';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
-const socket = io('http://localhost:5000');
-//从flask后端获取车辆位置信息
-const useCarLocation = () => {
-  const [carLocation, setCarLocation] = useState<Car | null>(null);
+const Client = () => {
+  const [serverMessage, setServerMessage] = useState(null);
+  const [receivedData, setReceivedData] = useState([]);
+
   useEffect(() => {
-    socket.on('carLocation', (car: Car) => {
-      setCarLocation(car);
+    const socket = io('ws://localhost:50000');
+
+    socket.on('from-server', (msg) => {
+      setServerMessage(msg);
+
+      // 解析接收到的 GPS 数据，并添加到 receivedData
+      const [latitude, longitude] = msg.split(',').map(parseFloat);
+      if (!isNaN(latitude) && !isNaN(longitude)) {
+        setReceivedData(prevData => [...prevData, { latitude, longitude }]);
+      }
     });
+
+    return () => {
+      socket.disconnect();
+    };
   }, []);
-  return carLocation;
-};
-//车辆位置的显示
-const CarLocation = () => {
-  const carLocation = useCarLocation();
-  const map = useMapEvents({
-    click() {
-      map.locate();
-    },
-    locationfound(e) {
-      map.flyTo(e.latlng, map.getZoom());
-    },
-  });
-  return carLocation === null ? null : (
-    <Marker position={carLocation.position}>
-      <Popup>Car is here</Popup>
-    </Marker>
+
+  return (
+    <MapContainer center={[23.174023, 113.408984]} zoom={20} style={{ height: '100vh', width: '200vh' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {receivedData.map((data, index) => (
+        <Marker key={index} position={[data.latitude, data.longitude]}>
+          <Popup>
+            Latitude: {data.latitude}<br />
+            Longitude: {data.longitude}
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
   );
 };
-//地图的显示
-const LocateCar = () => {
-    const position: LatLngTuple = [51.505, -0.09];
-    return (
-        <MapContainer center={position} zoom={20} scrollWheelZoom={true} style={{ height: '100vh', width : '200vh' }}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <CarLocation />
-        </MapContainer>
-    );
-};
-export default LocateCar;
+
+export default Client;
